5ba4337427ad82d4958e4aecaa2107b3960bdd00
JohannesJacob <johannes_jacob@gmx.de>
2017-07-07 13:40:12 +0200

    pulling coordinates

--- getSatImage.R
+++ getSatImage.R
@@ -1,26 +1,57 @@
 # getting satelite image
 library(ggmap)
+library (png)
+library(ggplot2)
 
-mapImageData1 <- get_map(location = c(lon = 13.50132
-                                      , lat = 52.52598),
-                         color = "color",
-                         source = "google",
-                         maptype = "satellite",
-                         zoom = 15
-                         )
+#put lineID
+streetOnly$lineID <- seq(1, nrow(streetOnly), by = 1)
+
+#for loop
+for (i in 1655:2200) {
+  longitude     <- streetOnly$lng[i]
+  latitude      <- streetOnly$lat[i]
+  
+  if (is.na(latitude)) next
+    
+  mapImageData1 <- get_map(location = c(lon = longitude, lat = latitude),
+                           color    = "color",
+                           source   = "google",
+                           maptype  = "satellite",
+                           zoom     = 15)
+  
+  ggsave(paste0("C:/Users/Johannes/Documents/SatelliteImages/Bild_", as.character(i),".png"),
+         ggmap(mapImageData1, 
+               extent = "device", 
+               ylab = "Latitude", 
+               xlab = "Longitude"))
+  
+  print(i)
+  print(Sys.time())
+  
+}  
+
+
+
+#extract plot
+m <- as.matrix(mapImageData1)
+
+# convert hex to rgb
+c <- col2rgb(m)
+
+# save image
+writePNG(c, target = "C:/Users/Johannes/Documents/SatelliteImages/Bild_test.png")
+
+
+r <- writePNG(c)
+
+#check plot extraction
 
 ggmap(mapImageData1,
       extent = "device",
       ylab = "Latitude",
       xlab = "Longitude")
 
-#extract plot
-m <- as.matrix(mapImageData1)
-
-#check plot extraction
 plot(1, type = "n", axes = FALSE, xlab = "", ylab = "")
 usr <- par("usr")
 rasterImage(m, usr[1], usr[3], usr[2], usr[4])
 
-# convert hex to rgb
-col2gb()
