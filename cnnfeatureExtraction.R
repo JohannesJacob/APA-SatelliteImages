df3385d453798594ec208487a0dde7d66c2a025e
JohannesJacob <johannes_jacob@gmx.de>
2017-08-29 09:28:40 +0200

    final feature extraction code

--- cnnfeatureExtraction.R
+++ cnnfeatureExtraction.R
@@ -4,12 +4,11 @@ library(keras)
 
 # Immo images read-in setup
 immo_directory <- "C:/Users/Johannes/Documents/ImmoImages"
-immo_samples <- length(list.files(immo_directory))
+immo_samples <- length(list.files(paste0(immo_directory, "/unknown")))
 batch_size <- 32
 
 immo_generator <- flow_images_from_directory(immo_directory, 
                                             generator = image_data_generator(),
-                                            classes = c("gut", "einfach", "mittel"), 
                                             class_mode = NULL,
                                             shuffle = F,
                                             batch_size = batch_size,
@@ -29,22 +28,23 @@ top_model %>%
   layer_dense(3) %>%
   layer_activation("softmax")
 load_model_weights_hdf5(top_model, "models/bottleneck_30_epochsR_IT.h5")
+#model_ft <- keras_model(inputs = model_vgg$input, outputs = top_model(model_vgg$output))
 
 # define layer that should be the output
 summary(top_model)
-layer_name <- 'activation_7'
+layer_name <- 'activation_9' # always has to be changed. see summary
 intermediate_layer_model <- keras_model(inputs=top_model$input,
                                  outputs= top_model$get_layer(layer_name)$output)
 
 # predict with the adjusted model 
-penultimate_output <- predict_generator(top_model,
-                                        immo_generator,
-                                        ceiling(immo_samples / batch_size),
-                                        verbose = 1 )
-
-##Testing
-test_result <- predict_generator(model_vgg,
-                                 immo_generator, 
+vgg_output <- predict_generator(model_vgg,
+                                immo_generator,
                                 ceiling(immo_samples / batch_size),
-                                verbose = 1       )
+                                verbose = 1 )
+
+penultimate_output <- predict(intermediate_layer_model,
+                              vgg_output)
+
+saveRDS(vgg_output, "models/vgg_pred_256.rds")
+saveRDS(penultimate_output, "models/penultimate_pred_256.rds")
 
